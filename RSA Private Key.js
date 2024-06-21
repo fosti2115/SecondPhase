b851985328ed5d7980e2f385a4589dde7a4e27d1
fosti2115 <mateusz.majstrak@edu.uekat.pl>
2024-06-21 17:36:56 +0200

    Create RSA Private Key.js

---
+++ RSA Private Key.js
@@ -0,0 +1,12 @@
+// Example RSA Private Key
+const RSA_PRIVATE_KEY = `-----BEGIN RSA PRIVATE KEY-----
+MIIBOgIBAAJBANeexampleRSAPRIVATEKEYexampleRSAPRIVATEKEYexampleRSAPRIVATEKEY
+exampleRSAPRIVATEKEYexampleRSAPRIVATEKEYexampleRSAPRIVATEKEYexampleRSA
+-----END RSA PRIVATE KEY-----`;
+
+function usePrivateKey() {
+    // Your RSA private key logic here
+    console.log("Using RSA Private Key: " + RSA_PRIVATE_KEY);
+}
+
+usePrivateKey();
