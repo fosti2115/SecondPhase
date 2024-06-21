4aeb96b5689758777cb1d30b1c7387ddc55a71c4
fosti2115 <mateusz.majstrak@edu.uekat.pl>
2024-06-21 17:35:51 +0200

    Create Google Cloud Keys.py

---
+++ Google Cloud Keys.py
@@ -0,0 +1,9 @@
+# Example Google Cloud API Key
+GOOGLE_CLOUD_API_KEY = 'AIzaSyD-xyz123_example_GCPkey_abcdef'
+
+def use_google_cloud_api():
+    # Your Google Cloud API logic here
+    print(f"Using Google Cloud API Key: {GOOGLE_CLOUD_API_KEY}")
+
+if __name__ == "__main__":
+    use_google_cloud_api()
