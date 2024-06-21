e5f1f622bf9e668dd5a4a05ac9369ce9d3c62774
fosti2115 <mateusz.majstrak@edu.uekat.pl>
2024-06-21 22:49:17 +0200

    Delete Google Cloud Keys.py

--- Google Cloud Keys.py
+++
@@ -1,9 +0,0 @@
-# Example Google Cloud API Key
-GOOGLE_CLOUD_API_KEY = 'AIzaSyDvbf32de9Abc5examplekeyZZZaaQRsQ0'
-
-def use_google_cloud_api():
-    # Your Google Cloud API logic here
-    print(f"Using Google Cloud API Key: {GOOGLE_CLOUD_API_KEY}")
-
-if __name__ == "__main__":
-    use_google_cloud_api()
