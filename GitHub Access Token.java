c8ee675c5bd5cc81e6262e7630b0ee081ce912a6
fosti2115 <mateusz.majstrak@edu.uekat.pl>
2024-06-21 17:36:36 +0200

    Create GitHub Access Token.java

---
+++ GitHub Access Token.java
@@ -0,0 +1,9 @@
+public class GitHubExample {
+    // Example GitHub Access Token
+    private static final String GITHUB_ACCESS_TOKEN = "ghp_ExampleGitHubAccessToken12345";
+
+    public static void main(String[] args) {
+        // Your GitHub API logic here
+        System.out.println("Using GitHub Access Token: " + GITHUB_ACCESS_TOKEN);
+    }
+}
