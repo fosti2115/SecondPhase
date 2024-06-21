99bf5fc5d5281cee0b102b7340b140537d6b093c
fosti2115 <mateusz.majstrak@edu.uekat.pl>
2024-06-21 21:15:25 +0200

    Update AWS Keys.cs

--- AWS Keys.cs
+++ AWS Keys.cs
@@ -3,8 +3,8 @@ using System;
 class Program
 {
     // Example AWS Access Key and Secret Key
-    private const string AWS_ACCESS_KEY = "AKIAExampleAWSAccessKey1234";
-    private const string AWS_SECRET_KEY = "abiaExampleAWSSecretKey1234567890ExampleAWSSecretKey";
+    private const string AWS_ACCESS_KEY = "ABIAK52LPFORPRUCRC22";
+    private const string AWS_SECRET_KEY = "abiaExampleAWSSecretKey1234567890Example";
 
     static void Main()
     {
