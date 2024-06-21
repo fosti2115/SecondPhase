0e03573817330a92b143dcc4991e0005f8818493
fosti2115 <mateusz.majstrak@edu.uekat.pl>
2024-06-21 22:01:55 +0200

    Create Amazon MWS Token.py

---
+++ Amazon MWS Token.py
@@ -0,0 +1,29 @@
+import requests
+
+mws_token = "amzn.mws.token.1234567890abcdef"
+
+def make_mws_request(token):
+    endpoint = "https://mws.amazonservices.com/Orders/2013-09-01"
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    params = {
+        "Action": "ListOrders",
+        "SellerId": "A2NEXAMPLETF53",
+        "MWSAuthToken": token
+    }
+
+    response = requests.get(endpoint, headers=headers, params=params)
+
+    if response.status_code == 200:
+        print("Request was successful!")
+        print("Response data:", response.text)
+    else:
+        print("Request failed!")
+        print("Status code:", response.status_code)
+        print("Error message:", response.text)
+
+make_mws_request(mws_token)
